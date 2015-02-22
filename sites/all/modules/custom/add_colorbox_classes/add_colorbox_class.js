3630d2c5bf3b4288f02efea91df2fd93b75ea087
gauravjeet <gauravjeet007@gmail.com>
2015-02-22 21:43:46 +0530

    theming front page

--- sites/all/modules/custom/add_colorbox_classes/add_colorbox_class.js
+++ sites/all/modules/custom/add_colorbox_classes/add_colorbox_class.js
@@ -1,3 +1,19 @@
 jQuery(document).ready(function() {
-  jQuery('a.add-colorbox-class').colorbox({width:700, height:500});
+  jQuery('.views-field-title a').addClass('add-colorbox-class').attr('id', 'movie-name');
+
+  // Add colorbox class to URLs.
+  jQuery('a.add-colorbox-class').colorbox({scrolling:false, width:650, height:500});
+
+  // Hide movie name.
+  jQuery('[id^="movie-name"]').hide();
+
+  jQuery('.view-content').hover(function() {
+    jQuery('[id^="movie-name"]').show().css('font-size', '14').css('position', 'absolute').animate({
+      bottom: "50px"
+    }, "slow");
+  },function() {
+    jQuery('[id^="movie-name"]').animate({
+      top: "-60px"
+    }, "slow").hide();
+  });
 });
